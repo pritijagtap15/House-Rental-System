fe08098c61da8bb0a1f07dd30a61e9ec6bc5c55b
pritijagtap15 <140197720+pritijagtap15@users.noreply.github.com>
2023-12-12 21:44:45 +0530

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,21 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					
+					<li><a href="#"><i class="fa fa-desktop"></i> House</a>
+					<ul>
+						<li><a href="create-room.php">Add a House</a></li>
+						<li><a href="manage-rooms.php">Manage House</a></li>
+
+					</ul>
+				</li>
+
+				<li><a href="manage-tenants.php"><i class="fa fa-users"></i>Manage Tenants</a></li>
+				<li><a href="Manage-payment.php"><i class="fa fa-users"></i>Manage Payment</a></li>
+				
+				
+
+			
+		</nav>
